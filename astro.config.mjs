30bdf7710ce13228f54ffc5f836f6f55432ec5a6
Susumu Noda <susumunoda@gmail.com>
2023-10-06 10:24:00 -0700

    Bulk convert tabs to spaces

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://susumunoda.github.io',
-	integrations: [mdx(), sitemap()],
+    site: 'https://susumunoda.github.io',
+    integrations: [mdx(), sitemap()],
 });
